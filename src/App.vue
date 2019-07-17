538380287c33b52b5dc11c2d8a46bc6fa0df2cd5
Damian Nuñez <damian_819@hotmail.com>
2019-07-17 13:33:59 -0600

    Upd - Calendar app
    - Updating repo with the calendar app main and calendar.vue.
    
    - Added axios to fetch holidays API data.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <div class="flex"><h1 class="actions-element-labels" >VUE</h1></div>
+        <div>
+            <div class="flex">
+                <label class="actions-element-labels">Start Date:</label>
+                <input class="actions-element-inputs" type="date" min="2000-01-01" max="2022-12-31" ref="dateInput" :value="currentDate" />
+            </div>
+            <div class="flex">
+                <label class="actions-element-labels">Number of dates:</label>
+                <input class="actions-element-inputs" ref="numberOfDaysInput" type="number" min="0" max="1095" placeholder="10" />
+            </div>
+            <div class="flex">
+                <label class="actions-element-labels">Country code:</label>
+                <input class="actions-element-inputs" ref="countryCodeInput" type="text" minlength="2" maxlength="2" placeholder="US" />
+            </div>
+        </div>
+        <div class="actions-button-container">
+            <button class="actions-button-style" @click="onClick">Submit</button>
+        </div>
+        <hr>
+        <Calendar :counter="counter" :selectedDate="currentDate" :numberOfDays="numberOfDays" :countryCode="countryCode"></Calendar>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calendar from  './components/Calendar'
+import moment from 'moment'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+    name: 'App' ,
+    data(){
+        return {currentDate : moment().format("YYYY-MM-DD"), numberOfDays:10, countryCode:'US', counter:0}
+    },
+    methods: {
+        onClick(){
+            this.currentDate = this.$refs.dateInput.value
+            this.numberOfDays = this.$refs.numberOfDaysInput.value
+            this.countryCode = this.$refs.countryCodeInput.value ? this.$refs.countryCodeInput.value : 'US' 
+            this.counter++; // intentionally left to force update
+        }
+    },
+    components : {
+        Calendar
+    },
   }
-}
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.actions-container{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+}
+
+.actions-element-labels{
+    flex: 0.1;
+    text-align: right;
+    margin-right: 10px;
+}
+
+.actions-element-inputs{
+    flex: 0.1;
+}
+
+.flex{
+    display: flex;
+}
+
+.actions-button-container{
+    display: flex;
+    width: 295px;
+    justify-content: flex-end;
+    margin: 10px 0;
+}
+
+.actions-button-style{
+    width: 100px;
 }
 </style>
---
+++ src/components/Calendar.vue
@@ -0,0 +1,193 @@
+<template>
+
+    <div>
+        <table v-if="ready" >
+
+            <div class="calendar-main-cal-container" v-for="(val, key) in calendarFormatedData" :key="key">
+                <tr>
+                    <td class="calendar-text-center" colspan="7">{{key.substr(0, 4)}}</td>
+                </tr>
+                <tr>
+                    <td class="calendar-text-center" colspan="7">{{Object.values(val[1])[0].monthName}}</td>
+                </tr>  
+                <tr>
+                    <th v-for="(days, index) in daysIndex" :key="index">{{days}}</th>
+                </tr>
+                <tr v-for="(weeks, keyMonths) in calendarFormatedData[key]" :key="keyMonths">
+                    <td v-for="(weekDay, keyWeeks) in calendarFormatedData[key][keyMonths]" 
+                        :key="keyWeeks" class="calendar-cell-width" 
+                        :title="weekDay.holidayName"
+                        :class="{'calendar-cell-isWeekend': weekDay.isWeekDay==false, 'calendar-cell-notInRange' : weekDay.isBetween==false, 'calendar-cell-inRange' : weekDay.isWeekDay==true, 'calendar-cell-isHoliday' : weekDay.isHoliday==true && weekDay.isBetween==true }"
+                    >
+                        {{showDate(weekDay.day, weekDay.isBetween)}}
+                    </td>
+                </tr>
+            </div>
+        </table>
+    </div>
+    
+</template>
+
+<script>
+import moment from 'moment';
+import _ from 'lodash';
+import axios from 'axios';
+
+const API_KEY = '518c9b2c-74b3-48d4-b8af-4cd5d9fb45e1';
+
+export default {
+    name: 'Calendar',
+
+    data(){
+        return { 
+            daysIndex : {0:'S', 1:'M', 2:'T', 3:'W', 4:'T', 5:'F', 6:'S'},
+            calendarData : null,
+            calendarFormatedData: null,
+            ready : false,
+            monthsToRender : 0,
+            yearsToRender : 0,
+            year: moment(this.selectedDate, 'YYYY/MM/DD').year(),
+            dataStartDate: null,
+            dataEndDate: null,
+            holidaysData: null,
+        }
+    },
+
+    methods: {
+        methodDate(date){
+            date = moment(date, 'YYYY/MM/DD')
+            let isBetween = false;
+            if (date.isAfter(this.dataStartDate)  && date.isBefore(this.dataEndDate)  || (date.isSame(this.dataStartDate)) ) { 
+                isBetween = true
+            }
+            let weekOfMonthValidated = moment(date, "YYYY/MM/DD").week() - moment(date, "YYYY/MM/DD").startOf('month').week()
+            weekOfMonthValidated = weekOfMonthValidated <0 ? 4 : weekOfMonthValidated
+            let isHoliday = false
+            let holidayName = null
+            for(let holidays in this.holidaysData){
+                if (moment(this.holidaysData[holidays].date).format("MMM-DD") == moment(date, 'YYYY/MM/DD').format('MMM-DD')){
+                    console.log("date: ", moment(date, 'YYYY/MM/DD').format('MMM-DD-YYYY'))
+                    isHoliday = true
+                    holidayName =  this.holidaysData[holidays].name
+                }
+            }
+
+            console.log(date, " - validated week: ", weekOfMonthValidated)
+            return { 
+                day: moment(date, 'YYYY/MM/DD').date(),
+                month: moment(date, 'YYYY/MM/DD').month()+1,
+                monthName: moment(date, 'YYYY/MM/DD ').startOf("month").format('MMMM'),
+                year: moment(date, 'YYYY/MM/DD').year(),
+                weekOfMonth: (moment(date, 'YYYY/MM/DD').startOf("month").format('MM')).toString() + (moment(date, "YYYY/MM/DD").week() - moment(date, "YYYY/MM/DD").startOf('month').week()).toString(), //isoWeek
+                weekOfMonthNormal: weekOfMonthValidated,
+                monthAndYear :  moment(date, 'YYYY/MM/DD').year().toString() + moment(date, 'YYYY/MM/DD ').startOf("month").format('MM').toString() ,
+                weekDay: moment(date, "YYYY/MM/DD").weekday(), //isoWeekday
+                weekDayLetter: this.daysIndex[moment(date, "YYYY/MM/DD").weekday()], //isoWeekday
+                isWeekDay: moment(date, "YYYY/MM/DD").weekday() == 0 || moment(date, "YYYY/MM/DD").weekday() == 6  ? false : true, //isoWeekday
+                isToday:  moment(date, "YYYY/MM/DD").format("YYYY/MM/DD") == moment().format("YYYY/MM/DD") ? true : false,
+                isHoliday: isHoliday,
+                holidayName: holidayName,
+                isBetween : isBetween
+            }
+        },
+        showDate(dayNumber, isBetween){
+            return dayNumber<1 || !isBetween ? '': dayNumber;
+        }
+
+    },
+
+    props:['selectedDate', 'numberOfDays', 'countryCode', 'counter'],
+
+    watch: {
+        'counter' : function(){
+
+            axios.get('https://holidayapi.com/v1/holidays', {
+                params:{
+                    key: API_KEY,
+                    country: this.countryCode,
+                    year: 2018
+                }
+            }).then(response=>{
+                this.holidaysData = response.data.holidays
+
+                let endDate = moment(this.selectedDate, 'YYYY/MM/DD').add('days', this.numberOfDays)
+                this.dataEndDate = endDate
+                let startDate = moment(this.selectedDate, 'YYYY/MM/DD')
+                this.dataStartDate = startDate
+
+                let yearsDiff = (moment(endDate, 'YYYY/MM/DD').year() - moment(this.selectedDate, 'YYYY/MM/DD').year())
+                let monthsCalc = ( moment(endDate, 'YYYY/MM/DD').month() - moment(this.selectedDate, 'YYYY/MM/DD').month() ) + (yearsDiff*12)
+                this.monthsToRender = monthsCalc +1
+                this.yearsToRender = yearsDiff
+
+                let dataArray = [];
+                for(let actualMonth = 0 ; actualMonth<=monthsCalc ; actualMonth++){
+                    let newDate = moment(this.selectedDate, 'YYYY/MM/DD').add('months', actualMonth)
+                    newDate = moment(newDate, "YYYY/MM/DD").format("YYYY/MM/DD")
+                    for(let days=1 ; days<=moment(newDate, "YYYY/MM/DD").daysInMonth(); days++){
+                        let data = this.methodDate(`${moment(newDate, "YYYY/MM/DD").year()}/${moment(newDate, "YYYY/MM/DD").month()+1}/${days}`)
+                        dataArray.push(data)
+                    }
+                }
+
+                let calendarByMonthAndYear = _.groupBy(dataArray, (el)=>{return el.monthAndYear})
+
+
+                for(let monthAndYear in calendarByMonthAndYear){
+                    calendarByMonthAndYear[monthAndYear] = _.groupBy(calendarByMonthAndYear[monthAndYear], (el)=>{ return el.weekOfMonthNormal})
+                    let lastIndex =  Object.values(calendarByMonthAndYear[monthAndYear]).length -1
+                    if(calendarByMonthAndYear[monthAndYear][0].length<7){
+                        let diffDates = Math.abs(calendarByMonthAndYear[monthAndYear][0].length - 7)
+                        for(let i=0; i<diffDates; i++){
+                            calendarByMonthAndYear[monthAndYear][0].unshift({day:-100+i, weekOfMonth:calendarByMonthAndYear[monthAndYear][0][0].weekOfMonth, monthAndYear: calendarByMonthAndYear[monthAndYear][0][0].monthAndYear, isBetween: false})
+                        }
+                    }
+                    if(calendarByMonthAndYear[monthAndYear][lastIndex].length<7){
+                        let diffDates = Math.abs(calendarByMonthAndYear[monthAndYear][lastIndex].length - 7)
+                        for(let i=0; i<diffDates; i++){
+                            calendarByMonthAndYear[monthAndYear][lastIndex].push({day:-100+i, weekOfMonth:calendarByMonthAndYear[monthAndYear][lastIndex][0].weekOfMonth, monthAndYear: calendarByMonthAndYear[monthAndYear][lastIndex][0].monthAndYear, isBetween: false})
+                        }
+                    }
+
+                }
+
+                this.calendarFormatedData = calendarByMonthAndYear;
+                this.ready=true
+                
+            })
+        },immediate: true, deep: true
+    }
+}
+</script>
+
+
+<style scoped>
+    .calendar-weekdays-style{
+        display: flex;
+    }
+
+    .calendar-text-center{
+        text-align: center;
+    }
+    .calendar-cell-width{
+        width: 30px;
+        height: 21px;
+        text-align: center;
+    }
+    .calendar-cell-isWeekend{
+        background-color: yellow;
+    }
+    .calendar-cell-notInRange{
+        background-color: lightgray !important;
+    }
+    .calendar-cell-inRange{
+        background-color: lightgreen;
+    }
+    .calendar-main-cal-container{
+        margin:20px 0;
+        border: 1px solid lightgray;
+    }
+    .calendar-cell-isHoliday{
+        background-color: lightpink !important;
+    }
+</style>
